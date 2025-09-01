46f78ba5e56ef826adbacc2700548747f9abef91
Subhan Shaikh <er.absubhanshaikh@gmail.com>
2025-09-01 13:38:53 +0530

    error solve

--- src/Config/db.js
+++ src/Config/db.js
@@ -7,6 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_DATABASE,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
+  String(process.env.DB_PASSWORD),
   {
     host: process.env.DB_HOST,
     dialect: 'postgres',
