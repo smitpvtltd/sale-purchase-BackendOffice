922f75494196d735e798e3df0f431ee9ff5800a0
Subhan Shaikh <er.absubhanshaikh@gmail.com>
2025-09-01 13:39:52 +0530

    change

--- src/Config/db.js
+++ src/Config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const sequelize = new Sequelize(
   process.env.DB_DATABASE,
   process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  // process.env.DB_PASSWORD,
   String(process.env.DB_PASSWORD),
   {
     host: process.env.DB_HOST,
